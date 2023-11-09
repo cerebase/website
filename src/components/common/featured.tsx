d03dd53670906de641725672a2f7089c8c4722b3
Frederik Bode <thorbensen@gmail.com>
2023-11-09 14:32:47 +0100

    feat: design tweaks

--- src/components/common/featured.tsx
+++ src/components/common/featured.tsx
@@ -1,18 +1,19 @@
 const image =
   "https://raw.githubusercontent.com/saga-labs/saga-gpt/main/public/assets/app.png";
+const image_new = "https://images.unsplash.com/photo-1699002971427-b3f9d306a8d1?q=80&w=3288&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const image_test = "https://66.media.tumblr.com/b219a23e0eda0e07c799fbf25e48be55/tumblr_ppakvyPDY31y9u9sko1_400.gifv";
 
 export const Featured = () => {
   return (
     <section
       className="overflow-hidden h-screen bg-cover bg-top bg-no-repeat bg-gradient-to-r from-indigo-300 to-purple-400 flex items-end"
-    //   style={{
-    //     backgroundImage: `url(${image})`,
-    //     textShadow: '4px 4px 2px rgba(150, 150, 150, 1)'
-    //   }}
+      style={{
+        backgroundImage: `url(${image_test})`,
+      }}
     >
       <div className="p-8 md:pb-24 lg:px-16 lg:pb-32">
         <div className="text-left">
-          <h2 className="text-lg font-bold text-purple-600">Featured</h2>
+          <h2 className="text-lg font-bold text-blue-600">Featured</h2>
           <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-4xl">
             SagaGPT: Revolutionize Your Desktop Conversations
           </h2>
--- src/components/common/testemonials.tsx
+++ src/components/common/testemonials.tsx
@@ -5,7 +5,7 @@ export const Testemonials = () => {
   return (
     <section className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-        <h2 className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        <h2 className="text-center text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
           Read trusted reviews from our customers
         </h2>
 
