1fe3c1b6dc5c274b2fbf3f55777679ca127389d7
Frederik Bode <thorbensen@gmail.com>
2023-11-09 16:27:48 +0100

    feat: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
+
+// Components
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
 
@@ -9,8 +11,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Saga Labs",
   description:
-    "European AI/NLP research lab focused on LLM Accessibility products",
+    "Copenhagen-based AI/NLP research lab dedicated to advancing LLM (Language Understanding, Learning, and Modeling) Accessibility products. Explore our innovative solutions today.",
+  keywords: ["Saga Labs", "LLM Accessibility", "AI research", "NLP products", "Copenhagen"],
+  authors: [{ name: "Saga Labs", url: "https://saga-labs.com" }],
 };
+
 
 export default function RootLayout({
   children,
