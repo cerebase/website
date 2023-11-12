3e7367487bc695a6292d11a3efda8b3d67de7ee4
Frederik Bode <thorbensen@gmail.com>
2023-11-12 13:21:46 +0100

    fixed: article width problem

--- src/app/(marketing)/about/page.tsx
+++ src/app/(marketing)/about/page.tsx
@@ -4,11 +4,14 @@ import clsx from "clsx";
 
 // prettier-ignore
 const image_1 = "https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-const image_2 = "https://images.unsplash.com/photo-1524758870432-af57e54afa26?q=80&w=2558&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-const image_3 = "https://images.unsplash.com/photo-1557804506-669a67965ba0?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const image_2 =
+  "https://images.unsplash.com/photo-1524758870432-af57e54afa26?q=80&w=2558&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const image_3 =
+  "https://images.unsplash.com/photo-1557804506-669a67965ba0?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 // components
 import { Article } from "@/components/common/article";
+import { ArticleWide } from "@/components/common/article-wide";
 
 // interfaces & types
 import { Article as ArticleType } from "@/interfaces/article";
@@ -44,14 +47,14 @@ export default function page() {
       >
         <div className="grid grid-cols-4 gap-2 h-full p-2">
           {/* row 1 */}
-          <Article content={mockArticleThree} width={2} />
-          <Article content={mockArticleOne} width={1} />
-          <Article content={mockArticleTwo} width={1} />
+          <ArticleWide content={mockArticleThree} />
+          <Article content={mockArticleOne} />
+          <Article content={mockArticleTwo} />
 
           {/* row 2 */}
-          <Article content={mockArticleOne} width={1} />
-          <Article content={mockArticleThree} width={1} />
-          <Article content={mockArticleTwo} width={2} />
+          <Article content={mockArticleOne} />
+          <Article content={mockArticleThree} />
+          <ArticleWide content={mockArticleTwo} />
         </div>
       </section>
 
